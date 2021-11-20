6484f0fc8e0857be0f3814617fc6a94d679cec4a
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-11-20 16:28:28 +0000

    fix rtl

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,13 @@
 import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
 import en from './locales/en.json';
+import ar from './locales/ar.json';
 
 function localize(): void {
   addMessages('en', en);
+  addMessages('ar', ar);
   init({
     fallbackLocale: 'en',
-    initialLocale: getLocaleFromNavigator(),
+    initialLocale: 'ar',
   });
 }
 
