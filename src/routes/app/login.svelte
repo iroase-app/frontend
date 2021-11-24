dffa38c92170e6ffd86b4875ec8db67a91b04e9c
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2021-11-24 15:05:43 +0000

    logging in now works :crab:

--- src/routes/app/login.svelte
+++ src/routes/app/login.svelte
@@ -2,9 +2,15 @@
   import { _ } from "svelte-i18n";
   import Input from "../../components/Input.svelte";
   import MdiAccountArrowRight from "virtual:icons/mdi/account-arrow-right";
+  import MdiLoading from "virtual:icons/mdi/loading";
+  import MdiWarning from "virtual:icons/mdi/warning";
+  import { fly } from "svelte/transition";
+  import getURL from "../../common/getURL";
 
   let username = "";
   let password = "";
+  let fetching = false;
+  let error: false | string = false;
 </script>
 
 <svelte:head>
@@ -27,14 +33,60 @@
       type="password"
     />
     <button
-      class="border-2 mt-6 w-full flex items-center text-pink rounded p-2 font-display hover:bg-purple-lighter transition-all"
-      on:click={() => alert(username)}
+      class="border-2 mt-6 w-full flex items-center text-pink rounded p-2 font-display transition-opacity
+    {(username && password && !fetching)
+        ? 'cursor-pointer hover:bg-purple-lighter'
+        : 'cursor-not-allowed opacity-10 bg-purple-lighter'}"
+      on:click={() => {
+        if (fetching === true) {
+          return;
+        }
+        fetching = true;
+        fetch(getURL("login"), {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username,
+            password,
+          }),
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.error) {
+              error = res.error;
+              fetching = false;
+            } else {
+              localStorage.setItem("session", res.session);
+              window.location.href = "/app";
+            }
+          });
+      }}
     >
-      <MdiAccountArrowRight class="reverse" />
+      {#if fetching}
+        <MdiLoading class="animate-spin" />
+      {:else}
+        <MdiAccountArrowRight class="reverse" />
+      {/if}
       <span class="ms-2">{$_("login.submit")}</span>
     </button>
+    {#if error}
+        <div
+          class="flex items-center mt-4"
+          transition:fly={{ duration: 300, y: 50 }}
+        >
+          <MdiWarning class="h-5 w-5 me-2" />
+          <div class="justify-between">
+            <span class="font-sans">{$_(`login.error.${error}`)}</span>
+          </div>
+        </div>
+      {/if}
 
-    <a class="block ms-2 mt-6 underline font-sans cursor-pointer " href="/app/register">
+    <a
+      class="block ms-2 mt-6 underline font-sans cursor-pointer "
+      href="/app/register"
+    >
       {$_("login.register")}
     </a>
   </main>
