3646f433cb78bf2ae05c544dfabd2e25805c225c
EmeraldSnorlax <emeraldsnorlax@disroot.org>
2022-02-26 21:08:55 +0000

    accessibility + localise for icons

--- src/routes/app/AccountTray.svelte
+++ src/routes/app/AccountTray.svelte
@@ -5,11 +5,12 @@
   import { fly } from "svelte/transition";
   import user from "./stores";
   import getURL from "../../common/getURL";
+import { _ } from "svelte-i18n";
   let expanded = false;
 </script>
 
 <div class="relative">
-  <button on:click={() => (expanded = !expanded)}>
+  <button title={$_('app.icons.accountTray')} on:click={() => (expanded = !expanded)}>
     <MdiAccountCircle class="h-8 w-8 m-2 cursor-pointer" />
   </button>
   {#if expanded}
@@ -17,7 +18,7 @@
       class="absolute flex flex-col z-50 border-t-2"
       transition:fly={{ y: -10, duration: 100 }}
     >
-      <button on:click={() => {}} transition:fly={{ y: -60, duration: 150 }}>
+      <button title={$_('app.icons.')} on:click={() => {}} transition:fly={{ y: -60, duration: 150 }}>
         <Settings class="h-8 w-8 m-2 cursor-pointer" />
       </button>
       <button
